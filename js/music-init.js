1744c967f2143ff0fcda3c080097916be38e2c5c
senri1248 <bca4247@gmail.com>
2026-09-07 21:17:06 -0500

    Site updated: 2026-09-07 21:17:06

--- js/music-init.js
+++ js/music-init.js
@@ -29,13 +29,13 @@
       {
         name: '天使',
         artist: '光良',
-        url: '/music/tianshi.mp3', // 这里放你歌曲的真实网络音频链接
+        url: 'Cicio.mp3', // 这里放你歌曲的真实网络音频链接
         cover: '/img/71365326c8964bb324aedac4e95c8564.jpg'                 // 歌曲封面
       },
       {
         name: '生如夏花',
         artist: '朴树',
-        url: 'https://example.com',
+        url: 'dyinglight.mp3',
         cover: '/img/71365326c8964bb324aedac4e95c8564.jpg'
       }
     ]
